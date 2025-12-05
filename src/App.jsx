dabd906ce276c65752106e577b366af57828c083
Sendil B <sendilb191@gmail.com>
2025-12-05 09:59:44 +0530

    Remove unnecessary TypeScript dependencies and clean up project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,8 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
-import SettingPage from './pages/SettingPage.jsx';
-import AboutPage from './pages/AboutPage.jsx';
-import Navigation from './components/Navigation.jsx';
-import { SettingsProvider } from './contexts/SettingsContext.jsx';
-import './styles/page.less';
-
-function AppContent() {
-  return (
-    <>
-      <Navigation />
-      <section>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/setting' element={<SettingPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </section>
-    </>
-  );
-}
 
 function App() {
   return (
-    <SettingsProvider>
-      <div className='app-container'>
-        <HashRouter
-          future={{
-            v7_startTransition: true,
-            v7_relativeSplatPath: true,
-          }}
-        >
-          <AppContent />
-        </HashRouter>
-      </div>
-    </SettingsProvider>
+    <div>App</div>
   );
 }
 
